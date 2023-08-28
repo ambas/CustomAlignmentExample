7dd5dfc6c96840115a8fff60803dbbbe2c916ec6
Ambas <ambaschobsanti@gmail.com>
2023-08-28 22:13:10 +0700

    Initial Commit

---
+++ CustomAlignmentExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomAlignmentExample
+//
+//  Created by Ambas Chobsanti on 28/8/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomAlignmentExample/CustomAlignmentExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAlignmentExampleApp.swift
+//  CustomAlignmentExample
+//
+//  Created by Ambas Chobsanti on 28/8/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct CustomAlignmentExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
