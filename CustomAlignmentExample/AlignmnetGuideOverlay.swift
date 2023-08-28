57bc7a83fa3896e78e8cf999d348b84347b4f325
Ambas <ambaschobsanti@gmail.com>
2023-08-28 22:17:09 +0700

    Initial commit

---
+++ CustomAlignmentExample/AlignmnetGuideOverlay.swift
@@ -0,0 +1,18 @@
+//
+//  AlignmnetGuideOverlay.swift
+//  CustomAlignmentExample
+//
+//  Created by Ambas Chobsanti on 28/8/2566 BE.
+//
+
+import SwiftUI
+
+struct AlignmnetGuideOverlay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlignmnetGuideOverlay()
+}
---
+++ CustomAlignmentExample/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  CustomAlignmentExample
+//
+//  Created by Ambas Chobsanti on 28/8/2566 BE.
+//
+
+import Foundation
